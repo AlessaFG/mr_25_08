601e8d622985d79df5bf219866554134ef11d763
AlessaFG <a.grande06192019@gmail.com>
2025-11-21 16:44:32 +0100

    IsaacSim Workstation

---
+++ run.sh
@@ -0,0 +1,10 @@
+xhost +
+
+docker run -it --rm --ipc host --privileged \
+ -v /tmp/.X11-unix:/tmp/.X11-unix \
+ -v ~/.Xauthority:/root/.Xauthority \
+ -e DISPLAY=$DISPLAY \
+ -e XAUTHORITY=$XAUTHORITY \
+ -v ./ros_ws/:/root/ros_workspace \
+ --name ros-isaac \
+ ros:isaac bash
