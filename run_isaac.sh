e80b69f416dbcdb05eaf1e8dcf533f964ce29a9c
AlessaFG <a.grande06192019@gmail.com>
2025-11-26 19:02:16 +0100

    Delete run_isaac.sh

--- run_isaac.sh
+++
@@ -1,9 +0,0 @@
-xhost +
-docker run -it --rm  --ipc host --privileged \
- -v /tmp/.X11-unix:/tmp/.X11-unix \
- -v ~/.Xauthority:/root/.Xauthority \
- -e DISPLAY=$DISPLAY \
- -e XAUTHORITY=$XAUTHORITY \
- -v ./ros_ws/:/root/ros_workspace \
- --name ros-isaac \
- ros:isaac bash
